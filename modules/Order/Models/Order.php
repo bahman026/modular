c4e981531ad0e4a3e7eb1a97c004bb6c51f25c23
Bahman026 <Bahman026@gmail.com>
2025-01-03 09:55:58 +0330

    setting up model factories
    Signed-off-by: Bahman026 <Bahman026@gmail.com>

--- modules/Order/Models/Order.php
+++ modules/Order/Models/Order.php
@@ -2,9 +2,17 @@
 
 namespace Modules\Order\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Modules\Order\Database\Factories\OrderFactory;
 
 class Order extends Model
 {
+    use HasFactory;
+
+    protected static function newFactory(): OrderFactory
+    {
+        return new OrderFactory;
+    }
     //
 }
--- modules/Order/Tests/Order/Models/OrderTest.php
+++ modules/Order/Tests/Order/Models/OrderTest.php
@@ -1,16 +1,15 @@
 <?php
 
-  namespace Modules\Order\Tests\Order\Models;
+namespace Modules\Order\Tests\Order\Models;
 
 use Modules\Order\Models\Order;
 use Modules\Order\Tests\Order\OrderTestCase;
 
 class OrderTest extends OrderTestCase
 {
-    public function test_it_creates_an_order():void
+    public function test_it_creates_an_order(): void
     {
-        $order = new Order();
+        $order = Order::factory()->create();
         $this->assertTrue(true);
     }
 }
-
